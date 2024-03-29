2be9ead87f8c6b16f3d25060e5a1c26ddd933419
Victor Dupré <dupre.victor@live.fr>
2024-03-29 11:00:57 +0100

    async component 2

--- src/app/articles/[id]/page.tsx
+++ src/app/articles/[id]/page.tsx
@@ -1,11 +1,37 @@
+import { Product } from "@/lib/types";
+import { notFound } from "next/navigation";
+
 type Props = Readonly<{
   params: {
     id: string;
   };
 }>;
 
-const Article = ({ params: { id } }: Props) => {
-  return <p>Article {id}</p>;
+const getArticle = async (id: string): Promise<Product> => {
+  try {
+    const res = await fetch("https://fakestoreapi.com/products/" + id);
+    const json = await res.json();
+    return json;
+  } catch (error) {
+    notFound();
+  }
+};
+
+const Article = async ({ params: { id } }: Props) => {
+  const product = await getArticle(id);
+  return (
+    <main>
+      <h1>{product.title}</h1>
+      <div>
+        <p> Price : {product.price.toFixed(2)}€ </p>
+        <p> Description : {product.description} </p>
+        <p> Category : {product.category} </p>
+        <p> Rating : {product.rating.rate} </p>
+        <p> Rating count : {product.rating.count} </p>
+        <img src={product.image} alt={product.title} />
+      </div>
+    </main>
+  );
 };
 
 export default Article;
